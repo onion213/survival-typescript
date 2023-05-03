0998387eecdd5beff35287364d6271bf08eca0fe
onion <kosukekito38@gmail.com>
2023-05-04 05:02:45 +0900

    feat: testing with jest and testing-library

---
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { SimpleButton } from './SimpleButton';
+
+test("ボタンをクリックするとON/OFFの表示が切り替わる", () => {
+  render(<SimpleButton />);
+  const simpleButton = screen.getByRole("button");
+  expect(simpleButton).toHaveTextContent('OFF');
+  userEvent.click(simpleButton);
+  expect(simpleButton).toHaveTextContent('ON');
+});
+
+test("描画されてすぐはOFFと表示されている", () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
+});
---
+++ component-test-tutorial/src/SimpleButton.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+ 
+export const SimpleButton: () => JSX.Element = () => {
+  const [state, setState] = useState(false);
+  const handleClick = () => {
+    setState((prevState) => !prevState);
+  };
+  return <button onClick={handleClick}>{state ? "ON" : "OFF"}</button>;
+};
+
